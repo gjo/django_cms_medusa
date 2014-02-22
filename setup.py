2f41536185620202537823ffe96d073c3f055748
OCHIAI, Gouji <gjo.ext@gmail.com>
2014-02-22 14:18:19 +0900

    Add Release documents.

--- setup.py
+++ setup.py
@@ -5,12 +5,27 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_cms_medusa',
-    description='django_medusa renderer for django_cms',
     version='0.1',
-    packages=find_packages(),
-    url='',
-    license='BSD',
+    description='django_medusa renderer for django_cms',
+    long_description='\n\n'.join([
+        open('README.rst').read(),
+        open('CHANGES.txt').read(),
+    ]),
     author='OCHIAI, Gouji',
     author_email='gjo.ext@gmail.com',
+    url='https://github.com/gjo/django_cms_medusa',
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=True,
     install_requires=('django_cms', 'django_medusa'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
 )
